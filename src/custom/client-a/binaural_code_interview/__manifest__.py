423689df3303c111712af53b2166b196eb61244f
xanderbits <AmarettoSyntax@protonmail.com>
2026-07-15 16:08:43 -0400

    [ADD] Scaffold inicial para prueba tecnica

---
+++ src/custom/client-a/binaural_code_interview/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'binaural_code_interview',
+    'summary': 'Modulo para prueba tecnica Binaural Julio 2026',
+    'description': 'Modulo para prueba tecnica Binaural Julio 2026',
+    'license': 'LGPL-3',
+    'author': 'Xanderbits - Alexander Amaro',
+    'website': 'https://www.binauraldev.com/home',
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ]
+}
+
---
+++ src/custom/client-a/binaural_code_interview/models/model_test.py
@@ -0,0 +1,6 @@
+from odoo import models, fields, api
+
+class TestModel(models.Model):
+    _name = 'test.model'
+    _description = 'model test'
+    
