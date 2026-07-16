59ed59c71b7501989987199e8a4e98fae4cec20e
xanderbits <AmarettoSyntax@protonmail.com>
2026-07-15 21:37:42 -0400

    [REMOVE] Archivos del scaffold inicial sin utilizar

--- src/custom/client-a/binaural_code_interview/models/__init__.py
+++ src/custom/client-a/binaural_code_interview/models/__init__.py
@@ -1,3 +1,2 @@
-from . import model_test
 from . import stock_operation_tag
 from . import product_template
--- src/custom/client-a/binaural_code_interview/models/model_test.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class TestModel(models.Model):
-    _name = 'test.model'
-    _description = 'model test'
-    
