364759e3e6afe8dcde69d80e079d1c8516bc70a3
xanderbits <AmarettoSyntax@protonmail.com>
2026-07-16 20:30:41 -0400

    [ADD] Modelo para reglas de descuento

--- src/custom/client-a/pos_schedule_discount_rules/models/pos_discount_rule.py
+++ src/custom/client-a/pos_schedule_discount_rules/models/pos_discount_rule.py
@@ -3,5 +3,40 @@ from odoo import models, fields, api
 
 class PosDiscountRule(models.Model):
     _name = 'pos.discount.rule'
+    _order = 'hour_from'
     _description = 'Regla de descuento por horario en el punto de venta'
+
+
+    name = fields.Char(
+        string='Nombre',
+        required=True
+    )
+    hour_from = fields.Float(
+        string='Hora de inicio',
+        default=0, 
+        required=True,
+        index=True
+    )
+    hour_to = fields.Float(
+        string='Hora de fin', 
+        default=0, 
+        required=True
+    )
+    
+    discount_percentage = fields.Float(
+        string='Porcentaje de descuento', 
+        digits=0, 
+        default=0.0,
+        required=True
+    )
+
+    _check_hour_from_hour_to = models.Constraint(
+        'CHECK(hour_from<hour_to)',
+        'La hora de inicio debe ser menor a la hora de fin'
+    )
+
+    _check_discount_percentage = models.Constraint(
+        'CHECK(discount_percentage>=0 AND discount_percentage<=100)',
+        'El porcentaje de descuento debe ser un número entre 0 y 100'
+    )
 
